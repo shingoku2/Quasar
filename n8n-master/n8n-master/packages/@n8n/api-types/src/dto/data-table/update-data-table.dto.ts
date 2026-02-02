f1049400f73a095ecb3d58de87fdd9ccf3000419
Edward <edward.garcia79429@gmail.com>
2026-02-02 14:52:44 -0600

    delete n8n master

--- n8n-master/n8n-master/packages/@n8n/api-types/src/dto/data-table/update-data-table.dto.ts
+++
@@ -1,7 +0,0 @@
-import { Z } from 'zod-class';
-
-import { dataTableNameSchema } from '../../schemas/data-table.schema';
-
-export class UpdateDataTableDto extends Z.class({
-	name: dataTableNameSchema,
-}) {}
--- n8n-master/n8n-master/packages/@n8n/api-types/src/dto/workflows/update-workflow.dto.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { Z } from 'zod-class';
-
-import { baseWorkflowShape } from './base-workflow.dto';
-
-export class UpdateWorkflowDto extends Z.class(
-	// Make all base fields optional for partial updates
-	z.object(baseWorkflowShape).partial().shape,
-) {}
